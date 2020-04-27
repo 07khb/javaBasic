c81486c900e187ac15f95e038c7b1f9b6414a7c1
khb <khb3661@naver.com>
2020-04-27 09:53:30 +0900

    add

--- BitReverseOperatorExample.java
+++ BitReverseOperatorExample.java
@@ -5,14 +5,17 @@ public class BitReverseOperatorExample {
 		int num2 = ~num1;
 		int num3 = num2 + 1;
 		
+		//java 
+		
 		String returnValue = toBinaryString(num1);
 		System.out.println(returnValue);
-		
+		//10진수를 2진수로 바꿈
 		String returnValue1 = toBinaryString(num2);
 		System.out.println(returnValue1);
-		
+		//위에거를 반전시킴
 		String returnValue2 = toBinaryString(num3);
 		System.out.println(returnValue2);
+		//첫번째거와 합치면 0
 	}
 	
 	public static String toBinaryString(int val) {
